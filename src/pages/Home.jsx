83e2be4632ef2573bfa851422628cf01ffabfe9f
Samar <bantmasr21@gmail.com>
2024-10-29 15:00:30 +0300

    create Home component to structure the main chat layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+// /src/pages/Home.jsx
+
+import React from "react";
+import Sidebar from "../Components/Sidebar"
+import Chat from "../Components/Chat"
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="container">
+                <Sidebar />
+                <Chat />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
